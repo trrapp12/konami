b32eb014d8c0d964b948f139e20829af0bec917e
Trevor Rapp <trrapp@gmail.com>
2021-11-20 20:51:35 -0700

    took out a lot of non-functioning code polluting the space to clean up and clarify

--- js/keylistener.js
+++ js/keylistener.js
@@ -7,39 +7,6 @@ var codeChain = [];
 
 document.addEventListener('keypress', (e) => {
 
-function addOrDelete (keypressed, codeChain) {
-
-}
-  if (myfontsize > 120) {
-      keytotal += 1;
-      x += 1;
-      myfontsize -= (x ** 2);
-      console.log (keytotal);
-      console.log (myfontsize);
-      const keypressed = event.key;
-
-      document.getElementById('background').innerHTML += keypressed;
-      // document.getElementById('background').style.fontSize = myfontsize
-      document.getElementById('display').innerHTML += keypressed;
-
-  }
-
-  else if (myfontsize < 120 && myfontsize > 50) {
-      keytotal += 1;
-      x += 1;
-      myfontsize -= x;
-      console.log (keytotal);
-      console.log (myfontsize);
-      const keypressed = event.key;
-      // document.getElementById('display').innerHTML += keypressed
-
-      document.getElementById('background').innerHTML += keypressed;
-      // document.getElementById('background').style.fontSize = myfontsize;
-      document.getElementById('background').style.textShadow = "0px 0px 5px rgba(0,225,0,0.9)";
-      document.getElementById('display').innerHTML += keypressed;
-  }
-
-  else {
       keytotal += 1;
       x = 0;
       myfontsize -= x;
@@ -53,7 +20,7 @@ function addOrDelete (keypressed, codeChain) {
       document.getElementById('background').style.textShadow = "0px 0px 3px rgba(0,225,0,0.9)";
       document.getElementById('display').innerHTML += keypressed;
 
-  }
+
 
   console.log(myfontsize);
 
